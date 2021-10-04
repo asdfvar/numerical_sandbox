b0973dc5d8db828fb954dd6c35eceba0e430d9c1
asdfvar <asdfvar@gmail.com>
2021-10-03 17:24:43 -0700

    incremental updates

--- raytracing/body.cpp
+++ raytracing/body.cpp
@@ -6,16 +6,29 @@
 int main (int argc, char *argv[])
 {
    const int num_stages = 2;
+   const int head_stage = 0;
 
    const int tiles[2] = {4, 1};
 
    comm::COMM2D Comm (&argc, &argv, num_stages, 1, tiles);
 
-   int value;
-   Comm.receive_from_stage (&value, sizeof (value), 0, 0, 0);
-   Comm.wait_for_receive_from_stage (0, 0, 0);
+   int num_cell_rows;
+   int num_cell_cols;
+   int row_offset;
+   float focal_length;
 
-   std::cout << "rank = " << value << std::endl;
+   Comm.receive_from_stage<float> (&focal_length,  sizeof (focal_length),  head_stage, 0, 0);
+   Comm.receive_from_stage<int>   (&num_cell_rows, sizeof (num_cell_rows), head_stage, 0, 1);
+   Comm.receive_from_stage<int>   (&num_cell_cols, sizeof (num_cell_cols), head_stage, 0, 2);
+   Comm.receive_from_stage<int>   (&row_offset,    sizeof (row_offset),    head_stage, 0, 3);
+   Comm.wait_for_receive_from_stage (head_stage, 0, 0);
+   Comm.wait_for_receive_from_stage (head_stage, 0, 1);
+   Comm.wait_for_receive_from_stage (head_stage, 0, 2);
+   Comm.wait_for_receive_from_stage (head_stage, 0, 3);
+
+   float *FPA = new float [num_cell_rows * num_cell_cols];
+
+   delete[] FPA;
 
    return 0;
 }
--- raytracing/head.cpp
+++ raytracing/head.cpp
@@ -3,6 +3,10 @@
 #include "vector.h"
 #include "object.h"
 
+#define NUM_CELL_ROWS 1000
+#define NUM_CELL_COLS 1000
+#define FOCAL_LENGTH  1.0f
+
 int main (int argc, char *argv[])
 {
    std::cout << std::endl;
@@ -11,13 +15,33 @@ int main (int argc, char *argv[])
 
    comm::COMM Comm (&argc, &argv, num_stages, 0);
 
-   std::cout << "test" << std::endl;
+   const int num_components = 4;
 
-   const int array_size = 4;
+   // Define the focal plane array (FPA)
+   float *FPA = new float [NUM_CELL_ROWS * NUM_CELL_COLS];
 
-   for (int rank = 0; rank < array_size; rank++) {
-      Comm.send_to_stage (&rank, sizeof (rank), 1, rank, 0);
+   {
+      int q = NUM_CELL_ROWS / num_components;
+      int r = NUM_CELL_ROWS % num_components;
+      int row_offset = 0;
+
+      float focal_length = FOCAL_LENGTH;
+
+      // Divide the focal plane array into its components
+      for (int rank = 0; rank < num_components; rank++) {
+         int num_cell_cols = NUM_CELL_COLS;
+         int num_cell_rows = q;
+         if (rank < r) num_cell_rows += 1;
+
+         Comm.send_to_stage<float> (&focal_length,  sizeof (focal_length),  1, rank, 0);
+         Comm.send_to_stage<int>   (&num_cell_rows, sizeof (num_cell_rows), 1, rank, 1);
+         Comm.send_to_stage<int>   (&num_cell_cols, sizeof (num_cell_cols), 1, rank, 2);
+         Comm.send_to_stage<int>   (&row_offset,    sizeof (row_offset),    1, rank, 3);
+         row_offset += num_cell_rows;
+      }
    }
+
+   delete[] FPA;
 
    return 0;
 }
