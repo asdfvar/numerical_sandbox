b6e628da96f6adce94cc9372825f48a9df808c49
asdfvar <asdfvar@gmail.com>
2021-10-03 17:55:54 -0700

    incremental updates

--- raytracing/body.cpp
+++ raytracing/body.cpp
@@ -7,26 +7,59 @@ int main (int argc, char *argv[])
 {
    const int num_stages = 2;
    const int head_stage = 0;
+   const int head_rank  = 0;
 
    const int tiles[2] = {4, 1};
 
    comm::COMM2D Comm (&argc, &argv, num_stages, 1, tiles);
 
-   int num_cell_rows;
-   int num_cell_cols;
-   int row_offset;
+   int   num_cell_rows;
+   int   num_cell_cols;
+   int   row_offset;
    float focal_length;
+   float window_width;
+   float window_height;
 
-   Comm.receive_from_stage<float> (&focal_length,  sizeof (focal_length),  head_stage, 0, 0);
-   Comm.receive_from_stage<int>   (&num_cell_rows, sizeof (num_cell_rows), head_stage, 0, 1);
-   Comm.receive_from_stage<int>   (&num_cell_cols, sizeof (num_cell_cols), head_stage, 0, 2);
-   Comm.receive_from_stage<int>   (&row_offset,    sizeof (row_offset),    head_stage, 0, 3);
-   Comm.wait_for_receive_from_stage (head_stage, 0, 0);
-   Comm.wait_for_receive_from_stage (head_stage, 0, 1);
-   Comm.wait_for_receive_from_stage (head_stage, 0, 2);
-   Comm.wait_for_receive_from_stage (head_stage, 0, 3);
+   Comm.receive_from_stage<float> (&focal_length,  sizeof (focal_length),  head_stage, head_rank, 0);
+   Comm.receive_from_stage<float> (&window_width,  sizeof (window_width),  head_stage, head_rank, 1);
+   Comm.receive_from_stage<float> (&window_height, sizeof (window_height), head_stage, head_rank, 2);
+   Comm.receive_from_stage<int>   (&num_cell_rows, sizeof (num_cell_rows), head_stage, head_rank, 3);
+   Comm.receive_from_stage<int>   (&num_cell_cols, sizeof (num_cell_cols), head_stage, head_rank, 4);
+   Comm.receive_from_stage<int>   (&row_offset,    sizeof (row_offset),    head_stage, head_rank, 5);
+
+   Comm.wait_for_receive_from_stage (head_stage, head_rank, 0);
+   Comm.wait_for_receive_from_stage (head_stage, head_rank, 1);
+   Comm.wait_for_receive_from_stage (head_stage, head_rank, 2);
+   Comm.wait_for_receive_from_stage (head_stage, head_rank, 3);
+   Comm.wait_for_receive_from_stage (head_stage, head_rank, 4);
+   Comm.wait_for_receive_from_stage (head_stage, head_rank, 5);
+
+   const float cell_width  = window_width  / static_cast<float> (num_cell_cols);
+   const float cell_height = window_height / static_cast<float> (num_cell_rows);
 
    float *FPA = new float [num_cell_rows * num_cell_cols];
+
+//    z   y
+//    ^   ^
+//    |  /
+//    | /
+//    |/
+//    /
+//    +----> x (look vector)
+
+   for (int row_cell = row_offset, row_ind = 0; row_ind < num_cell_rows; row_cell++, row_ind++) {
+      for (int col_cell = 0; col_cell < num_cell_cols; col_cell++) {
+
+         // Determine the pointing vector as it passes through the center of the FPA cell
+         float z_offset =  0.5f * window_height - 0.5f - cell_height * row_cell;
+         float y_offset = -0.5f * window_width  + 0.5f + cell_width * col_cell;
+         float x_offset = focal_length;
+
+         vec::Vector<float> pointing_vector (x_offset, y_offset, z_offset);
+         pointing_vector.normalize ();
+
+      }
+   }
 
    delete[] FPA;
 
--- raytracing/head.cpp
+++ raytracing/head.cpp
@@ -6,6 +6,8 @@
 #define NUM_CELL_ROWS 1000
 #define NUM_CELL_COLS 1000
 #define FOCAL_LENGTH  1.0f
+#define WINDOW_WIDTH  1.0f
+#define WINDOW_HEIGHT 1.0f
 
 int main (int argc, char *argv[])
 {
@@ -25,7 +27,9 @@ int main (int argc, char *argv[])
       int r = NUM_CELL_ROWS % num_components;
       int row_offset = 0;
 
-      float focal_length = FOCAL_LENGTH;
+      float focal_length  = FOCAL_LENGTH;
+      float window_width  = WINDOW_WIDTH;
+      float window_height = WINDOW_HEIGHT;
 
       // Divide the focal plane array into its components
       for (int rank = 0; rank < num_components; rank++) {
@@ -34,9 +38,11 @@ int main (int argc, char *argv[])
          if (rank < r) num_cell_rows += 1;
 
          Comm.send_to_stage<float> (&focal_length,  sizeof (focal_length),  1, rank, 0);
-         Comm.send_to_stage<int>   (&num_cell_rows, sizeof (num_cell_rows), 1, rank, 1);
-         Comm.send_to_stage<int>   (&num_cell_cols, sizeof (num_cell_cols), 1, rank, 2);
-         Comm.send_to_stage<int>   (&row_offset,    sizeof (row_offset),    1, rank, 3);
+         Comm.send_to_stage<float> (&window_width,  sizeof (window_width),  1, rank, 1);
+         Comm.send_to_stage<float> (&window_height, sizeof (window_height), 1, rank, 2);
+         Comm.send_to_stage<int>   (&num_cell_rows, sizeof (num_cell_rows), 1, rank, 3);
+         Comm.send_to_stage<int>   (&num_cell_cols, sizeof (num_cell_cols), 1, rank, 4);
+         Comm.send_to_stage<int>   (&row_offset,    sizeof (row_offset),    1, rank, 5);
          row_offset += num_cell_rows;
       }
    }
